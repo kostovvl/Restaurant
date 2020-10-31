d2559aa1b5760ab3b61e5b426a7b2479ebb9da78
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-10-31 18:36:24 +0200

    messages

--- API/Core/src/main/java/restaurant/core/configuration/Global.java
+++ API/Core/src/main/java/restaurant/core/configuration/Global.java
@@ -3,7 +3,8 @@ package restaurant.core.configuration;
 public class Global {
 
     public static final String Try_Again_Message = "An Unexpected Error occurred. Please Try Again!";
-    public static final String Product_Deleted_Message = "Product %s has been successfully deleted!";
-    public static final String Category_Deleted_Message = "Category %s and all products have been successfully deleted!";
+    public static final String Product_Deleted_Message = "Product \"%s\" has been successfully deleted!";
+    public static final String Category_Deleted_Message = "Category \"%s\" and all containing " +
+            "products have been successfully deleted!";
 
 }
