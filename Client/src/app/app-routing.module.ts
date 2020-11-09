fe046ad53f43ac711eea7d0ad595abba2f199c10
kostovvl <53002640+kostovvl@users.noreply.github.com>
2020-11-09 10:17:25 +0200

    auth module/component

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthenticationComponent } from 'src/app/components/auth/authentication/authentication.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component:  AuthenticationComponent}]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+ }
